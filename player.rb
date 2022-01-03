c3b493fc84433005dac5a63a501930b5263ef76a
Hashim Sharif <sharif.hashim@gmail.com>
2022-01-03 19:36:45 +0000

    initial commit

---
+++ player.rb
@@ -0,0 +1,17 @@
+Class Player 
+attr_accessor :name, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def lost
+    @lives == 0
+  end
+  
+end
---
+++ question.rb
@@ -0,0 +1,16 @@
+class Question
+  def initialize(name)
+    @num1 = rand(1..20)
+    @num2 = rand(1...20)
+    @sum = @num1 + @num2
+  end
+
+  def ask_question(name)
+    puts "#{name}: What is the sum of #{@num1} + #{@num2}"
+  end
+
+  def answer(input)
+    @sum == input
+  end
+  
+end
